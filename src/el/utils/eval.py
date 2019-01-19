14669273a21751602e6c0bcb70023aaddcb11f78
Minho Lee <pathmaker@kaist.ac.kr>
2019-01-19 17:49:20 +0900

    refactoring

--- src/el/utils/eval.py
+++ src/el/utils/eval.py
@@ -10,7 +10,7 @@ def eval(module, corpus_dir):
 			eval_target.append(j)
 
 	prediction = module.predict(eval_target, form="CROWDSOURCING")
-	jsondump(prediction, "debug_prediction.json")
+	jsondump(prediction, "debug/debug_prediction.json")
 	correct_count = 0
 	error_count = 0
 	wrong_count = [0, 0, 0, 0]
@@ -26,8 +26,11 @@ def eval(module, corpus_dir):
 			entity["fileName"] = fname
 			if predict_entity == answer:
 				correct_count += 1
+			elif predict_entity == "NOT_IN_CANDIDATE" and answer == "DARK_ENTITY":
+				correct_count += 1
 			else:
-				if answer not in ["NOT_AN_ENTITY", "NOT_IN_CANDIDATE"]:
+
+				if answer not in ["NOT_AN_ENTITY", "NOT_IN_CANDIDATE", "EMPTY_CANDIDATES", "DARK_ENTITY"]:
 					if answer in entity["candidates"]:
 						wrong_count[0] += 1
 						del entity["candidates"]
@@ -36,23 +39,25 @@ def eval(module, corpus_dir):
 						wrong_count[1] += 1
 						del entity["candidates"]
 						wrong_list[1].append(entity)
-				elif answer == "NOT_IN_CANDIDATE" and predict_entity != "NOT_AN_ENTITY":
+				elif answer == "DARK_ENTITY" and predict_entity not in ["NOT_AN_ENTITY", "NOT_IN_CANDIDATE"]:
 					wrong_count[2] += 1
 					del entity["candidates"]
-					wrong_list[1].append(entity)
+					wrong_list[2].append(entity)
 				elif answer == "NOT_AN_ENTITY" and predict_entity != "NOT_IN_CANDIDATE":
 					wrong_count[3] += 1
 					del entity["candidates"]
-					wrong_list[1].append(entity)
+					wrong_list[3].append(entity)
 
 	entity_count = sum(map(lambda x: len(x["entities"]), eval_target))
 	result = {
 		"Total": entity_count,
 		"Correct": correct_count,
 		"Error": error_count,
-		"Wrong": sum(wrong_count),
+		"Wrong": {"Type %d" % (i+1): len(wrong_list[i]) for i in range(len(wrong_list))},
 		"Wrong Result": {"Type %d" % (i+1): wrong_list[i] for i in range(len(wrong_list))}
 	}
-	with open("eval_result.json", "w", encoding="UTF8") as f:
+	with open("debug/eval_result.json", "w", encoding="UTF8") as f:
 		json.dump(result, f, ensure_ascii=False, indent="\t")
 	print("Acc: %.2f%%" % (correct_count / entity_count * 100))
+	for i in range(len(wrong_list)):
+		print("Type %d Error: %d" % (i+1, wrong_list[i]))
